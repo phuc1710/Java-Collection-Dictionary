45b474ba7aae114d78f8b60059d4167a6b60c451
phuc1710 <63486411+phuc1710@users.noreply.github.com>
2022-01-04 01:03:15 +0700

    #1: Initialize project w main menu
    - DONE: Main menu w try catch & switch case to functions
    - TODO: implement file read and 1st function

---
+++ src/MainMenu.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class MainMenu {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Slang Dictionary"
+                + "\n----------------"
+                + "\n1. Search by slang word"
+                + "\n2. Search by definition"
+                + "\n3. Show slang search history"
+                + "\n4. Add a slang word"
+                + "\n5. Edit a slang word"
+                + "\n6. Delete a slang word"
+                + "\n7. Reset default slang words list"
+                + "\n8. On this day slang word (random)"
+                + "\n9. Quiz (random slang)"
+                + "\n10. Quiz (random definition)"
+                + "\n11. Quit"
+                + "\n----------------");
+        System.out.print("Choose your option by type in the number: ");
+        while (true) {
+            String option = sc.nextLine();
+            try {
+                Integer optionInteger = Integer.parseInt(option);
+                // !!Line below is for testing only. Further removal is needed.
+                System.out.println("Your choice is: " + optionInteger);
+                switch (optionInteger) {
+                    case 1:
+                        // TODO: implement search by slang word
+                        break;
+                    case 2:
+                        // TODO: implement search by definition
+                        break;
+                    case 3:
+                        // TODO: implement show slang search by history
+                        break;
+                    case 4:
+                        // TODO: implement add a slang word
+                        break;
+                    case 5:
+                        // TODO: implement edit a slang word
+                        break;
+                    case 6:
+                        // TODO: implement delete a slang word
+                        break;
+                    case 7:
+                        // TODO: implement reset default slang words list
+                        break;
+                    case 8:
+                        // TODO: implement on this day slang word (random)
+                        break;
+                    case 9:
+                        // TODO: implement quiz (random slang)
+                        break;
+                    case 10:
+                        // TODO: implement quiz (random definition)
+                        break;
+                    case 11:
+                        return;
+                    default:
+                        System.out.print("Input must be from 1 to 10.\nPlease type in again: ");
+                        continue;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.print("Wrong input.\nPlease type in again: ");
+            }
+        }
+
+        sc.close();
+    }
+}
